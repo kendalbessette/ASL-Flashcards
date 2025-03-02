240296570393467ba841b6f3942acd1cb8778a85
Kendal Bessette <87912955+kendal-bessette@users.noreply.github.com>
2025-03-01 20:55:25 -0500

    Install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import React from "react";
+
 async function getFlashcards() {
   const NEXT_HYGRAPH_ENDPOINT = process.env.NEXT_HYGRAPH_ENDPOINT;
   if (NEXT_HYGRAPH_ENDPOINT) {
@@ -27,24 +48,58 @@ interface Card {
   youtubeLink: string;
 }
 
-export default async function Home() {
-  const { flashcard } = await getFlashcards();
-  console.log(flashcard);
+export default function Home() {
+  const { setTheme } = useTheme();
+  const [flashcard, setFlashcard] = React.useState<Card[]>([]);
+
+  React.useEffect(() => {
+    getFlashcards().then((data) => {
+      setFlashcard(data?.flashcard || []);
+    });
+  }, []);
   return (
     <div className="m-12">
-      <h1 className="text-5xl font-bold mb-4">
-        Hygraph Implementation Guides demo
-      </h1>
-      <ul>
-        {flashcard.map((card: Card) => {
-          const { term, youtubeLink } = card;
-          return (
-            <>
-              <div>{card.term}</div>
-              <div>{card.youtubeLink}</div>
-            </>
-          );
-        })}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ul className="flex justify-center items-center">
+        <Card className="w-96">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {flashcard.map((card: Card) => {
+              const { term, youtubeLink } = card;
+              return (
+                <>
+                  <div>{card.term}</div>
+                  <div>{card.youtubeLink}</div>
+                </>
+              );
+            })}
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
       </ul>
     </div>
   );
